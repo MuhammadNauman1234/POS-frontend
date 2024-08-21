f24fd7944d36bb168b9645866e64cd3df5e80f98
Muhammad Nauman <nkbangash03@gmail.com>
2024-08-21 12:49:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Hakeem Saib</Button>
+    </>
+  );
+}
+
+export default App;
